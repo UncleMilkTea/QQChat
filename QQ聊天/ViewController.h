fb10418b31d9f3e6d23e8ee6e27ce72e41a756df
houyukun <274334911@qq.com>
2016-04-08 14:15:52 +0800

    QQChat
    仿QQ聊天界面

---
+++ QQ聊天/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QQ聊天
+//
+//  Created by 侯玉昆 on 15/12/18.
+//  Copyright © 2015年 侯玉昆. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ README.md
@@ -0,0 +1,13 @@
+#QQChat
+仿QQ聊天界面
+
+
+![](/Snip20160408_2.png)
+
+
+## Author
+
+Weibo: [@确实草蛋][1]
+http://weibo.com/caoeggs/home
+
+Github: [houyukun][2]
